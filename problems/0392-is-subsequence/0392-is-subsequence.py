f51d7e87b540dc089035ba29358a452e7cf0fd81
Poojaa1 <70262949+Poojaa1@users.noreply.github.com>
2024-08-29 15:53:38 +0000

    Is Subsequence (Python)

---
+++ problems/0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i,j=0,0
+        while j < len(t):
+            if i<len(s) and s[i]==t[j]:
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return len(s)==i
